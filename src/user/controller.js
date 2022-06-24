a5fa4361d356ba66a61bf14834c28947255560eb
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-06-24 21:05:10 +0300

    fix: minor fixes to response output

--- src/user/controller.js
+++ src/user/controller.js
@@ -10,25 +10,26 @@ async function register(req, res){
   try{
       let salt =  await bcrypt.genSalt(8);
       let hashPassword = await bcrypt.hash(req.body.password, salt);
-      try {let addUserResult = await addUser(req.body, hashPassword)}
-      catch (err) {return res.status(400).send({'message': err.code ===11000 ? 'Email must be unique' : Object.values(err.errors).map(val => val.message)})};
+      let addUserResult;
+      try {addUserResult = await addUser(req.body, hashPassword)}
+      catch (err) {return res.status(400).json({message: err.code ===11000 ? 'Email is already taken.' : Object.values(err.errors).map(val => val.message.substring(11))})};
       return res.status(200).send({
       'message': 'User added succesfully',
       'id': addUserResult._id,
       });
     } 
     catch (err) {
-      return res.status(500).send(err);
+      return res.status(500).json(err);
     }
 }
 
 async function login(req, res) {
   try {
     const user = await getByEmail(req.body.email);
-    if (!user) return res.status(400).send({'message':'Invalid credentials'});
+    if (!user) return res.status(400).json({message:'Invalid credentials'});
 
     const validPassword = await bcrypt.compare(req.body.password, user.password);
-    if (!validPassword) return res.status(400).send({'message':'Invalid credentials'});
+    if (!validPassword) return res.status(400).json({message:'Invalid credentials'});
 
     const token = jwt.sign(
       {_id: user._id, name: user.name, email: user.email},
@@ -40,7 +41,7 @@ async function login(req, res) {
     });
   }
   catch (err) {
-    return(res.srarys(500).send(err))
+    return(res.srarys(500).json(err))
   }
 }
 
